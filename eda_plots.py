383cf3f843302fd7cdc8632c07a61bac09e70e53
Jonathan Hersch <jbhersch@gmail.com>
2017-08-23 15:09:44 -0600

    commented eda_plots.py

--- eda_plots.py
+++ eda_plots.py
@@ -1,3 +1,9 @@
+'''
+DESCRIPTION:
+    eda_plots.py is a script that contains several methods to generate plots used
+    during the exploration phase of this project.
+'''
+
 import pandas as pd
 import numpy as np
 from scipy.stats import gaussian_kde
@@ -14,36 +20,49 @@ sns.set_style("whitegrid")
 
 def dist_of_charging_vs_not():
     '''
-    INPUT:
-        - x: x-coordinates (ndarray)
-        - y: y-coordinates (ndarray)
-        - pad: number of data points to pad the x, y vectors with (int)
-        - thresh: threshold used for noise removal (float)
-    OUTPUT: None
+    INPUT: None
+    OUTPUT:
+        A histogram and kernel density estimator that show the average increase
+        in power consumption when electric vehicles are charging compared to the
+        average power consumption for all houses with electric vehicles.
     '''
+    # initialize vector that will contain the mean power consumption for each
+    # house with an electric vehicle while they are charging
     mean_energy_when_charging = np.zeros(electric_car_labels.shape[0])
     mean_energy_not_charging = np.zeros(electric_car_labels.shape[0])
 
+    # loop through each house that has an electric vehicle
     for n in xrange(electric_car_labels.shape[0]):
-        increments = np.where(electric_car_labels[n,:]==1)[0]
-        mean_energy_when_charging[n] = np.mean(electric_car_features[n,increments])
+        # get indices where the house of interest is charging
+        charge_indices = np.where(electric_car_labels[n,:]==1)[0]
+        # take the mean power value while charging for the house of interest
+        mean_energy_when_charging[n] = np.mean(electric_car_features[n,charge_indices])
         off_increments = np.where(electric_car_labels[n,:]==0)[0]
         mean_energy_not_charging[n] = np.mean(electric_car_features[n,off_increments])
 
+    # take the difference in average power while charging from overall average power
+    # for each house with an electric vehicle
     x_fit = (mean_energy_when_charging - np.mean(electric_car_features,axis=1))
     # x_fit = mean_energy_when_charging - mean_energy_not_charging
+    # define a range of average power differences to be used with the kde
     x_plot = np.linspace(np.min(x_fit), np.max(x_fit), 1000)
 
+    # initialize the kde
     kde = gaussian_kde(x_fit)
+    # evaluate the kde on the values that will be used for the plot
     kde_plot = kde(x_plot)
 
+    # find indices of kde_plot where local maximums occur
     peak_indices = peakutils.indexes(kde_plot)
 
     fig = plt.figure(figsize=(15,10))
     ax = fig.add_subplot(111)
 
+    # create histogram for x_fit
     ax.hist(x_fit, bins=25, normed=True, alpha=0.5, label="Histogram")
+    # plot the kde over the histogram
     ax.plot(x_plot, kde_plot, label="KDE")
+    # label peak values
     ax.plot(x_plot[peak_indices], kde_plot[peak_indices], 'rX', label="Local Maxima")
 
     text_string = "Power $\Delta$ = {0}".format(np.round(x_plot[peak_indices[0]], 2))
@@ -74,26 +93,37 @@ def dist_of_charging_vs_not():
 def average_power_usage(days=None, save_figure=False, show_day_labels=False):
     '''
     INPUT:
-        - x: x-coordinates (ndarray)
-        - y: y-coordinates (ndarray)
-        - pad: number of data points to pad the x, y vectors with (int)
-        - thresh: threshold used for noise removal (float)
-    OUTPUT: None
+        - days: list start and end day to shown in plot (int list)
+        - save_figure: boolean that says whether to save the figure or not (bool)
+        - show_day_labels: boolean on whether or not to show the day number and
+            abbreviation for each day shown in the plot (bool)
+    OUTPUT:
+        A plot comparing the average power use of all houses, houses with electric
+        cars, and houses without electric cars over a designated time span.
     '''
+    # take average of all houses
     energy = np.mean(feature_matrix, axis=0)
+    # average of houses with electric cars
     car_energy = np.mean(electric_car_features, axis=0)
+    # average of houses without electric cars
     no_car_energy = np.mean(no_electric_car_features, axis=0)
 
+    # if days is None then plot across the entire time span
     if days is None:
         days = [0, 59]
 
+    # first time increment index to be shown
     na = 48*days[0]
+    # second time increment index to be shown
     nb = 1 + 48*(days[1] + 1)
 
+    # make sure nb isn't a larger number than the total number of time increments
     if nb > len(energy):
         nb = len(energy)
 
+    # enumerate time increments
     increments = np.arange(len(energy))
+    # convert time increment enumeration into time in days (48 half hour increments per day)
     time = 1.*increments/48
 
     fig = plt.figure(figsize=(15,10))
@@ -132,29 +162,44 @@ def average_power_usage(days=None, save_figure=False, show_day_labels=False):
 def single_house_power_usage(house, days=None, save_figure=False, show_day_labels=False):
     '''
     INPUT:
-        - x: x-coordinates (ndarray)
-        - y: y-coordinates (ndarray)
-        - pad: number of data points to pad the x, y vectors with (int)
-        - thresh: threshold used for noise removal (float)
-    OUTPUT: None
+        - house: house index for plot (int)
+        - days: list start and end day to shown in plot (int list)
+        - save_figure: boolean that says whether to save the figure or not (bool)
+        - show_day_labels: boolean on whether or not to show the day number and
+            abbreviation for each day shown in the plot (bool)
+    OUTPUT:
+        A plot showing power use of a single house over a designated time span.
+        In the case where the house has an electric vehicle, the points where
+        the vehicle is charging are labeled.
     '''
+    # get house id from house index
     house_id = features["House ID"][house].astype(int)
+    # get power use for the house of interest
     energy = feature_matrix[house, :]
+    # get charge labels for the house of interest
     charges = label_matrix[house, :]
 
+    # enumerate time increments
     increments = np.arange(len(energy))
+    # convert time increment enumeration into time in days (48 half hour increments per day)
     time = 1.*increments/48
+    # get indices where vehicles are charging
     charge_indices = np.where(charges==1)[0]
 
+    # if days is None then plot over entire time span
     if days is None:
         days = [0, 59]
 
+    # first time increment index to be shown
     na = 48*days[0]
+    # second time increment index to be shown
     nb = 1 + 48*(days[1] + 1)
 
+    # make sure nb isn't greater than total number of time increments
     if nb > len(energy):
         nb = len(energy)
 
+    # get charge indices within the time range that will be shown on the plot
     charge_ab = charge_indices[(charge_indices>=na) & (charge_indices<nb)]
 
     fig = plt.figure(figsize=(15,10))
@@ -189,18 +234,18 @@ def single_house_power_usage(house, days=None, save_figure=False, show_day_label
 
 def compare_ave_power_use_to_cars_charging():
     '''
-    INPUT:
-        - x: x-coordinates (ndarray)
-        - y: y-coordinates (ndarray)
-        - pad: number of data points to pad the x, y vectors with (int)
-        - thresh: threshold used for noise removal (float)
-    OUTPUT: None
+    INPUT: None
+    OUTPUT:
+        A dual axis plot with the average power consumption of all houses in the
+        training data and the total number of vehicles charging at each time interval.
     '''
     fig = plt.figure(figsize=(15,10))
     ax1 = fig.add_subplot(111)
 
     increments = np.arange(feature_matrix.shape[1])
     time = increments/48.
+
+    # set up left y-axis with average power consumption
     ax1.plot(time, np.mean(feature_matrix, axis=0), color="blue")
     ax1.set_xlabel("Time (Days)", fontsize=20, color="black")
     ax1.set_ylabel("Average Power Usage", fontsize=20, color="blue")
@@ -212,6 +257,7 @@ def compare_ave_power_use_to_cars_charging():
     ax1.xaxis.grid(False, which="major")
     ax1.set_xlim(0,60)
 
+    # set up right y-axis with number of vehicles charging
     ax2 = ax1.twinx()
     ax2.plot(time, np.sum(label_matrix, axis=0), color="red", alpha=0.5)
     ax2.set_ylabel("Number of Cars Charging", fontsize=20, color="red")
